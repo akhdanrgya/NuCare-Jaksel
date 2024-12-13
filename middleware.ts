4c57d5facc632edd7c023cc6d52ad3ed6ddaf91c
akhdanrgya <akhdan.anargya@gmail.com>
2024-12-13 21:55:27 +0700

    Pindahin file middleware ke root

--- middleware.ts
+++ middleware.ts
@@ -9,15 +9,12 @@ export async function middleware(req: NextRequest) {
   const { data: { session } } = await supabase.auth.getSession()
 
   if (!session) {
-    // Kalau nggak ada session, redirect ke login
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
-  // Kalau ada session, lanjut ke halaman yang diminta
   return res
 }
 
-// Matcher buat middleware (hanya untuk dashboard)
 export const config = {
   matcher: ['/dashboard/:path*'],
 }
