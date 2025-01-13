be445481bb78e21756c3bd1d60e617384581ac11
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-13 21:53:11 +0700

    deploy lah

--- src/libs/midtrans.ts
+++ src/libs/midtrans.ts
@@ -1,6 +1,6 @@
 import midtransClient from 'midtrans-client'
 
-let snap = new midtransClient.Snap({
+const snap = new midtransClient.Snap({
     isProduction: false,
     serverKey: process.env.NEXT_PUBLIC_MIDTRANS_SERVER_KEY || ''
 })
