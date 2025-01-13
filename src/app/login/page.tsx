4f9d34ea12fa669d524bff716013989756926fe5
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-13 21:45:36 +0700

    Ready to deploy staging

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { supabase } from '@/libs/supabaseClient'
 import { useRouter } from 'next/navigation'
 import Login from '@/components/Login'
@@ -21,30 +21,9 @@ const LoginPage = () => {
     return () => {
       listener.subscription.unsubscribe()
     }
-  }, [])
+  }, [router])
 
   return (
-    // <div>
-    //   <h1>Login</h1>
-    //   <form onSubmit={handleLogin}>
-    //     <input
-    //       type="email"
-    //       placeholder="Email"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //     />
-    //     <input
-    //       type="password"
-    //       placeholder="Password"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //     <button type="submit" disabled={loading}>
-    //       {loading ? 'Logging in...' : 'Login'}
-    //     </button>
-    //   </form>
-    //   {error && <p style={{ color: 'red' }}>{error}</p>}
-    // </div>
     <Login/>
   )
 }
