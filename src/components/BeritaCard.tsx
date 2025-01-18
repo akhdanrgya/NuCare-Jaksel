c1fbc472124562566f2e79c32d0df5c72b6fa6e8
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-18 21:33:12 +0700

    Berita lainnya

--- src/components/BeritaCard.tsx
+++ src/components/BeritaCard.tsx
@@ -23,38 +23,50 @@ const BeritaCard: React.FC = () => {
     router.push(`/berita/${id}`)
   }
 
-  return (
-    <section className="py-24 bg-gray-100">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center mb-8">
-        <h2 className="text-4xl">Berita</h2>
-      </div>
+  const handleBeritaClick = () => {
+      router.push("/berita");
+  }
 
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {berita.length > 0 ? (
-          berita.map((data, idx) => (
-            <div
-              className="bg-white p-6 rounded-lg shadow-md border border-gray-200 transition transform hover:-translate-y-2 hover:shadow-lg cursor-pointer"
-              key={idx}
-              onClick={() => handleCardClick(data.id)}
-            >
-              <Image
-                src={data.image}
-                alt={data.judul}
-                className="w-full h-48 object-cover rounded-t-lg"
-                height={200}
-                width={300}
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-semibold text-black mb-2">{data.judul}</h3>
-                <p className="text-gray-700 mb-2">{format(new Date(data.created_at), "dd MMMM yyyy")}</p>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>Tidak ada berita saat ini</p>
-        )}
-      </div>
-    </section>
+  return (
+      <section className="py-24 bg-gray-100">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center mb-8">
+              <h2 className="text-4xl">Berita</h2>
+          </div>
+
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+              {berita.length > 0 ? (
+                  berita.map((data, idx) => (
+                      <div
+                          className="bg-white p-6 rounded-lg shadow-md border border-gray-200 transition transform hover:-translate-y-2 hover:shadow-lg cursor-pointer"
+                          key={idx}
+                          onClick={() => handleCardClick(data.id)}
+                      >
+                          <Image
+                              src={data.image}
+                              alt={data.judul}
+                              className="w-full h-48 object-cover rounded-t-lg"
+                              height={200}
+                              width={300}
+                          />
+                          <div className="p-4">
+                              <h3 className="text-lg font-semibold text-black mb-2">{data.judul}</h3>
+                              <p className="text-gray-700 mb-2">{format(new Date(data.created_at), "dd MMMM yyyy")}</p>
+                          </div>
+                      </div>
+                  ))
+              ) : (
+                  <p>Tidak ada berita saat ini</p>
+              )}
+          </div>
+          <div className="container mx-auto text-center mt-8">
+              <button
+                  className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded"
+                  onClick={() => handleBeritaClick()}
+              >
+                  Berita Lainnya
+              </button>
+          </div>
+      </section>
   );
 };
 
