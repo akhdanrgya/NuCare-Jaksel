54f57d6ad26e4d20d043d9a9c17a5063a89377aa
ardav <111423884+ardavv@users.noreply.github.com>
2025-01-18 20:59:21 +0700

    popover berita

---
+++ src/components/dashboard/Berita/FormBerita.tsx
@@ -0,0 +1,173 @@
+"use client"
+import React, { useState, useEffect } from "react";
+import Card from "@/components/dashboard/Berita";
+import InputGroup from "@/components/FormElements/InputGroup";
+import { supabase } from "@/libs/supabaseClient";
+import { Session } from "@supabase/supabase-js";
+import { fetchRecentUser, UserType } from "@/data/user";
+
+const FormBerita = () => {
+    const [tittle, setTittle] = useState<string>("");
+    const [imageUrl, setImageUrl] = useState<string>("");
+    const [article, setArticle] = useState<string>("");
+    const [session, setSession] = useState<Session | null>(null);
+    const [user, setUser] = useState<UserType | null>(null);
+
+    useEffect(() => {
+        const fetchSession = async () => {
+            const { data, error } = await supabase.auth.getSession()
+            if (error) {
+                console.error("Error fetching session:", error);
+            } else {
+                console.log("Fetched session:", data.session);
+                setSession(data.session);
+            }
+        }
+        fetchSession();
+    }, []);
+
+    useEffect(() => {
+        const getUser = async () => {
+            if (session?.user.id) {
+                const userData = await fetchRecentUser(session.user.id);
+                if (userData) {
+                    setUser(userData[0] || null);
+                } else {
+                    console.log("User tidak ditemukan");
+                }
+            }
+        };
+
+        if (session) {
+            getUser();
+        }
+    }, [session]);
+
+    const uploadFile = async (file: File) => {
+        const fileName = `${Date.now()}-${file.name}`;
+        const { error: uploadError } = await supabase.storage
+            .from("beritaimage")
+            .upload(fileName, file);
+
+        if (uploadError) {
+            console.error("Error uploading file:", uploadError);
+            alert("Gagal mengunggah gambar");
+            setImageUrl("https://placehold.co/300x200")
+            return null;
+        }
+
+        const { data: publicUrlData } = supabase.storage
+            .from("beritaimage")
+            .getPublicUrl(fileName);
+
+        return publicUrlData?.publicUrl || null;
+    };
+
+    const insertBerita = async (uploadedImageUrl: string | null) => {
+        const { data, error } = await supabase.from("berita").insert([
+            {
+                judul: tittle,
+                article: article,
+                author_name: user?.username,
+                image: uploadedImageUrl || imageUrl,
+            }
+        ])
+
+        if (error) {
+            console.error("Error adding berita:", error);
+            alert("Gagal menyimpan data berita");
+        } else {
+            alert("Data berita berhasil disimpan");
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!tittle.trim()) {
+            alert("Judul tidak boleh kosong!");
+            return;
+        }
+
+        if (!article.trim()) {
+            alert("Artikel tidak boleh kosong!");
+            return;
+        }
+
+        const fileInput = document.querySelector<HTMLInputElement>('input[type="file"]');
+        const file = fileInput?.files?.[0];
+        if (!file) {
+            alert("Harap unggah gambar!");
+            return;
+        }
+
+        try {
+            let uploadedImageUrl: string | null = "";
+            if (file) {
+                uploadedImageUrl = await uploadFile(file);
+            }
+
+            await insertBerita(uploadedImageUrl);
+        } catch (err) {
+            console.error("Error handling submit berita:", err);
+            alert("Terjadi kesalahan saat menyimpan data berita");
+        }
+    };
+
+
+    return (
+        <div className="w-full h-full absolute top-0 backdrop-filter backdrop-brightness-75 backdrop-blur-md">
+            <div
+                className="rounded-[10px] border border-stroke bg-white shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card mb-10">
+                <div className="border-b border-stroke px-6.5 py-4 dark:border-dark-3">
+                    <h3 className="font-semibold text-dark dark:text-white">Form Donasi</h3>
+                </div>
+                <form action="#" onSubmit={handleSubmit}>
+                    <div className="p-6.5">
+                        <div className="mb-4.5 flex flex-col gap-4.5 xl:flex-row">
+                            <InputGroup
+                                label="Judul"
+                                type="text"
+                                placeholder="Masukan Judul"
+                                customClasses="w-full xl:w-1/2"
+                                value={tittle}
+                                onChange={(e) => setTittle(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+                                Detail Atau Article
+                            </label>
+                            <textarea
+                                rows={6}
+                                placeholder="Masukan Artikel"
+                                className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5 py-3 text-dark outline-none transition placeholder:text-dark-6 focus:border-primary active:border-primary disabled:cursor-default dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+                                value={article}
+                                onChange={(e) => setArticle(e.target.value)}
+                            ></textarea>
+                        </div>
+                        <div className="mb-6">
+                            <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+                                Upload Gambar
+                            </label>
+                            <input
+                                type="file"
+                                className="w-full cursor-pointer rounded-[7px] border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-[#E2E8F0] file:px-6.5 file:py-[13px] file:text-body-sm file:font-medium file:text-dark-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-dark dark:border-dark-3 dark:bg-dark-2 dark:file:border-dark-3 dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+                                placeholder="."
+                            />
+                        </div>
+                        <button
+                            className="flex w-full justify-center rounded-[7px] bg-primary p-[13px] font-medium text-white hover:bg-opacity-90"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
+}
+
+export default FormBerita
--- src/components/dashboard/Donasi/index.tsx
+++ src/components/dashboard/Donasi/index.tsx
@@ -4,6 +4,7 @@ import Card from "./Card";
 import InputGroup from "../../FormElements/InputGroup";
 import SelectKategori from "../../FormElements/SelectGroup/SelectKategori";
 import { supabase } from "../../../libs/supabaseClient";
+import { DatePicker } from "@nextui-org/date-picker";
 
 const Donasi = () => {
   const [target, setTarget] = useState<string>("");
@@ -19,7 +20,7 @@ const Donasi = () => {
   const formatNumber = (value: string) => {
     const numericValue = value.replace(/\D/g, "");
     const formattedValue = new Intl.NumberFormat("id-ID").format(
-        parseInt(numericValue || "0")
+      parseInt(numericValue || "0")
     );
     return formattedValue;
   };
@@ -33,8 +34,8 @@ const Donasi = () => {
   const uploadFile = async (file: File) => {
     const fileName = `${Date.now()}-${file.name}`;
     const { error: uploadError } = await supabase.storage
-        .from("donationimage")
-        .upload(fileName, file);
+      .from("donationimage")
+      .upload(fileName, file);
 
     if (uploadError) {
       console.error("Error uploading file:", uploadError);
@@ -44,8 +45,8 @@ const Donasi = () => {
     }
 
     const { data: publicUrlData } = supabase.storage
-        .from("donationimage")
-        .getPublicUrl(fileName);
+      .from("donationimage")
+      .getPublicUrl(fileName);
 
     return publicUrlData?.publicUrl || null;
   };
@@ -94,101 +95,102 @@ const Donasi = () => {
   };
 
   return (
-      <div>
-        <div className="rounded-[10px] border border-stroke bg-white shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card mb-10">
-          <div className="border-b border-stroke px-6.5 py-4 dark:border-dark-3">
-            <h3 className="font-semibold text-dark dark:text-white">Form Donasi</h3>
-          </div>
-          <form action="#" onSubmit={handleSubmit}>
-            <div className="p-6.5">
-              <div className="mb-4.5 flex flex-col gap-4.5 xl:flex-row">
-                <InputGroup
-                    label="Judul"
-                    type="text"
-                    placeholder="Masukan Judul"
-                    customClasses="w-full xl:w-1/2"
-                    value={tittle}
-                    onChange={(e) => setTittle(e.target.value)}
-                />
-                <InputGroup
-                    label="URL"
-                    type="text"
-                    placeholder="Masukan URL"
-                    customClasses="w-full xl:w-1/2"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
-                />
-              </div>
-              <InputGroup
-                  label="Lokasi"
-                  type="text"
-                  placeholder="Masukan Lokasi"
-                  customClasses="mb-4.5"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-              />
-              <div className="mb-6">
-                <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
-                  Deskripsi
-                </label>
-                <textarea
-                    rows={6}
-                    placeholder="Masukan Deskripsi"
-                    className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5 py-3 text-dark outline-none transition placeholder:text-dark-6 focus:border-primary active:border-primary disabled:cursor-default dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
-                    value={desc}
-                    onChange={(e) => setDesc(e.target.value)}
-                ></textarea>
-              </div>
-              <SelectKategori onChange={(id) => setKategoriId(id)} />
-              <InputGroup
-                  label="Target"
-                  type="text"
-                  placeholder="Masukan Target Donasi"
-                  customClasses="mb-4.5"
-                  value={target}
-                  onChange={handleTargetChange}
-              />
-              <InputGroup
-                  label="Tenggat Waktu"
-                  type="text"
-                  placeholder="Masukan Tenggat Waktu (yyyy-mm-dd)"
-                  customClasses="mb-4.5"
-                  value={deadLine}
-                  onChange={(e) => setDeadLine(e.target.value)}
-              />
-              <div className="mb-6">
-                <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
-                  Detail Atau Article
-                </label>
-                <textarea
-                    rows={6}
-                    placeholder="Masukan Artikel"
-                    className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5 py-3 text-dark outline-none transition placeholder:text-dark-6 focus:border-primary active:border-primary disabled:cursor-default dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
-                    value={article}
-                    onChange={(e) => setArticle(e.target.value)}
-                ></textarea>
-              </div>
-              <div className="mb-6">
-                <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
-                  Upload Gambar
-                </label>
-                <input
-                    type="file"
-                    className="w-full cursor-pointer rounded-[7px] border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-[#E2E8F0] file:px-6.5 file:py-[13px] file:text-body-sm file:font-medium file:text-dark-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-dark dark:border-dark-3 dark:bg-dark-2 dark:file:border-dark-3 dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
-                    placeholder="."
-                />
-              </div>
-              <button
-                  className="flex w-full justify-center rounded-[7px] bg-primary p-[13px] font-medium text-white hover:bg-opacity-90"
-                  type="submit"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+    <div>
+      <div className="rounded-[10px] border border-stroke bg-white shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card mb-10">
+        <div className="border-b border-stroke px-6.5 py-4 dark:border-dark-3">
+          <h3 className="font-semibold text-dark dark:text-white">Form Donasi</h3>
         </div>
-        <Card />
+        <form action="#" onSubmit={handleSubmit}>
+          <div className="p-6.5">
+            <div className="mb-4.5 flex flex-col gap-4.5 xl:flex-row">
+              <InputGroup
+                label="Judul"
+                type="text"
+                placeholder="Masukan Judul"
+                customClasses="w-full xl:w-1/2"
+                value={tittle}
+                onChange={(e) => setTittle(e.target.value)}
+              />
+              <InputGroup
+                label="URL"
+                type="text"
+                placeholder="Masukkan URL"
+                customClasses="w-full xl:w-1/2"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+              />
+            </div>
+            <InputGroup
+              label="Lokasi"
+              type="text"
+              placeholder="Masukan Lokasi"
+              customClasses="mb-4.5"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <div className="mb-6">
+              <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+                Deskripsi
+              </label>
+              <textarea
+                rows={6}
+                placeholder="Masukan Deskripsi"
+                className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5 py-3 text-dark outline-none transition placeholder:text-dark-6 focus:border-primary active:border-primary disabled:cursor-default dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+              ></textarea>
+            </div>
+            <SelectKategori onChange={(id) => setKategoriId(id)} />
+            <InputGroup
+              label="Target"
+              type="text"
+              placeholder="Masukan Target Donasi"
+              customClasses="mb-4.5"
+              value={target}
+              onChange={handleTargetChange}
+            />
+
+            <div className="w-full max-w-xl flex flex-row gap-4 mb-4.5 ">
+              <DatePicker
+                showMonthAndYearPickers
+                label="Tenggat Waktu"
+                variant="bordered"
+                onChange={(date) => setDeadLine(date?.toString().split("T")[0] || "")}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+                Detail Atau Article
+              </label>
+              <textarea
+                rows={6}
+                placeholder="Masukan Artikel"
+                className="w-full rounded-[7px] border-[1.5px] border-stroke bg-transparent px-5 py-3 text-dark outline-none transition placeholder:text-dark-6 focus:border-primary active:border-primary disabled:cursor-default dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary"
+                value={article}
+                onChange={(e) => setArticle(e.target.value)}
+              ></textarea>
+            </div>
+            <div className="mb-6">
+              <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+                Upload Gambar
+              </label>
+              <input
+                type="file"
+                className="w-full cursor-pointer rounded-[7px] border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-[#E2E8F0] file:px-6.5 file:py-[13px] file:text-body-sm file:font-medium file:text-dark-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-dark dark:border-dark-3 dark:bg-dark-2 dark:file:border-dark-3 dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+                placeholder="."
+              />
+            </div>
+            <button
+              className="flex w-full justify-center rounded-[7px] bg-primary p-[13px] font-medium text-white hover:bg-opacity-90"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
+      <Card />
+    </div>
   );
 };
 
