9f472549fce9a01af9aa13f632288ca3161c3ff2
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-18 21:21:13 +0700

    href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header: React.FC = () => {
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center p-4 px-40 py-6">
-        <a className="text-xl font-bold" href="/public">NU CARE JAKARTA SELATAN</a>
+        <a className="text-xl font-bold" href="/">NU CARE JAKARTA SELATAN</a>
         <nav className="space-x-4">
           <a href="/public" className="text-gray-700 hover:text-green-500">Home</a>
           <a href="/program" className="text-gray-700 hover:text-green-500">Program</a>
