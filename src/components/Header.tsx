7ab85e6da7b488f588c8e2bf204119a39acfb680
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-18 21:22:11 +0700

    href navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto flex justify-between items-center p-4 px-40 py-6">
         <a className="text-xl font-bold" href="/">NU CARE JAKARTA SELATAN</a>
         <nav className="space-x-4">
-          <a href="/public" className="text-gray-700 hover:text-green-500">Home</a>
+          <a href="/" className="text-gray-700 hover:text-green-500">Home</a>
           <a href="/program" className="text-gray-700 hover:text-green-500">Program</a>
           <a href="/berita" className="text-gray-700 hover:text-green-500">Berita</a>
           <a href="/dashboard" className="text-gray-700 hover:text-green-500">Dashboard</a>
