3fa2e62666d0e5a38a260e8365028f2420671d0f
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-20 00:10:49 +0700

    Update readu to merge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <section className="py-24 ">
-            <div className="">
-                <h1>PROFILE INI</h1>
+        <section className="py-24">
+            <div className="container">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col">gambar</div>
+                    <div className="col-12">anjay</div>
+                </div>
             </div>
         </section>
     )
