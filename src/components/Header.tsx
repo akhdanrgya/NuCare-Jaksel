56931c9bb91ed1edb964cdea2ee33c4a334f61e3
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-22 23:28:15 +0700

    Update Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
 "use client"
+import {Montserrat} from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-montserrat",
+});
+
+
 const Header: React.FC = () => {
   return (
-    <header className="bg-white shadow-md">
+    <header className={`${montserrat.variable} font-montserrat bg-white shadow-md`}>
       <div className="container mx-auto flex justify-between items-center p-4 px-40 py-6">
         <div>
-          <a><img src="/images/logo/logoNUCare.png" className="h-30 w-50" alt="" /></a>
+          <a><img src="/images/logo/logoNUCare.png" className="h-10 w-auto" alt="" /></a>
         </div>
         <nav className="space-x-4">
           <a href="/" className="text-gray-700 hover:text-green-500">Home</a>
