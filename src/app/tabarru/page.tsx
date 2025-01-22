394af8380adb55adee459ef8c203428c36098d61
akhdanrgya <akhdan.anargya@gmail.com>
2025-01-23 00:06:13 +0700

    stagging 5

--- src/app/tabarru/page.tsx
+++ src/app/tabarru/page.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client";
 
-import PaymentDonation from "@/components/PaymentDonation"
+import PaymentDonation from "@/components/PaymentDonation";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const TabarruForm = () => {
     const searchParams = useSearchParams();
-    const data = searchParams.get("wealth"); // Mengambil query parameter wealth
+    const [wealth, setWealth] = useState<string | null>(null);
 
-    console.log(`nilai wealth di URL: ${data}`)
-    // Ensure 'data' is a string (if it's an array, pick the first item)
-    const wealth = Array.isArray(data) ? data[0] : data;
+    useEffect(() => {
+        const data = searchParams.get("wealth");
+        console.log(`nilai wealth di URL: ${data}`);
+        setWealth(Array.isArray(data) ? data[0] : data);
+    }, [searchParams]);
 
-    return (
-        <PaymentDonation wealth={wealth} />
-    )
-}
+    // Jangan render `PaymentDonation` sampai wealth siap
+    if (!wealth) return <div>Loading...</div>;
+
+    return <PaymentDonation wealth={wealth} />;
+};
 
 export default TabarruForm;
